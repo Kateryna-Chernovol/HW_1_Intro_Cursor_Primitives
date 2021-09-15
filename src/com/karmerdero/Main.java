b48a06b55c90432bc1760132bff1bf85a10a7002
Kateryna-Chernovol <schasliva@yahoo.com>
2021-09-15 21:29:52 +0300

    Initial commit

---
+++ src/com/karmerdero/Main.java
@@ -0,0 +1,17 @@
+package com.karmerdero;
+
+public class Main {
+
+    public static void main(String[] args) {
+        byte newByte = 3;
+        short newShort = 50;
+        int newInt = 10;
+        long newLong = 10L;
+        float newFloat = 15.5F;
+        double newDouble = 1684.55;
+        System.out.println(newByte + newDouble);
+        System.out.println(newShort - newFloat);
+        System.out.println(newInt * newLong);
+        System.out.println(newDouble / newInt);
+    }
+}
